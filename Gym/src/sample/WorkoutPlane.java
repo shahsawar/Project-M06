3ab4bf5b15f2e6af215b79856c3eb908acf74d72
Ronald <ronaldintriago52@gmail.com>
2021-03-09 17:06:44 +0100

    Classes from older project added

---
+++ Gym/src/sample/WorkoutPlane.java
@@ -0,0 +1,37 @@
+package sample;
+
+/***
+ * @author shah
+ */
+public class WorkoutPlane {
+
+    private String name;
+    private int frequency;
+
+    /***
+     *
+     * @param name
+     * @param frequency
+     */
+    public WorkoutPlane(String name, int frequency) {
+        this.name = name;
+        this.frequency = frequency;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+}
