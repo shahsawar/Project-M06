54529312a1f61da217cfeec8c9953bd10848bb08
Ronald <ronaldintriago52@gmail.com>
2021-03-12 17:03:58 +0100

    Branch for jbdc code created

---
+++ Gym/src/main/java/db/DAOUserJDBC.java
@@ -0,0 +1,31 @@
+package db;
+
+import clases.User;
+
+import java.util.List;
+
+public class DAOUserJDBC implements DAOUser{
+    @Override
+    public void insert(User user) {
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+
+    @Override
+    public User getByIdentifier(Integer integer) {
+        return null;
+    }
+}
--- Gym/src/main/java/module-info.java
+++ Gym/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.github.gym {
     requires javafx.controls;
     requires javafx.fxml;
     requires mongo.java.driver;
+    requires java.sql;
 
     opens com.github.gym to javafx.fxml;
     exports com.github.gym;
